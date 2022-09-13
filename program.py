a45af968517bab7653d2980071c959f0603c955b
Rikvbr <r.vanbaar@vva-informatisering.nl>
2022-09-13 12:38:31 +0200

    testcommit

--- program.py
+++ program.py
@@ -1,7 +1,17 @@
-"""Testbestand voor pyhton"""
+"""Testbestand voor python"""
+
+
+
+class Person:
+    """ Docstring """
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
 
 def testcase():
     """Testfunctie"""
     print("test")
 
+p1 = Person("Rik", 18)
+print(p1.age)
 print(testcase.__doc__)
